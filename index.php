151f32b96c886b3d17d920ac719bc238446eaafe
hsalayay <hsalayay@gmail.com>
2025-05-19 12:21:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 
     <!-- CSS Link -->
     <!--<link rel="stylesheet" href="forms.css">-->
-     <!-- Bootstrap 5 CDN -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap 5 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Font Awesome for Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
